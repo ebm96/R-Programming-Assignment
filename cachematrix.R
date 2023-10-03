3c91a8d5e20713bf49e58e66f49b1e99d3d78d49
ebm96 <146867374+ebm96@users.noreply.github.com>
2023-10-03 20:29:14 +0330

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,1 +1,35 @@
+# The makeCacheMatrix() function is a custom function that enables the creation of a specialized matrix object with caching and retrieval capabilities. 
+# This object is designed to store matrix data and perform calculations on it efficiently.
+# Using these functions, you can assign or update the matrix data, retrieve the stored data, calculate and store the inverse of the matrix, and retrieve the inverse from the cache. 
+# This approach is particularly useful for repetitive calculations on the same matrix as it avoids unnecessary recalculations and improves performance.
+# By employing makeCacheMatrix(), you can store intermediate results in the cache, enhancing performance. 
+# For example, if you need to calculate the inverse of a matrix multiple times, you can use this function to save the inverse in the cache after computing it once. 
+# Subsequent requests for the inverse will then avoid unnecessary calculations, resulting in improved efficiency.
 
+> makeCacheMatrix <- function(x=matrix()){
++ n1 <-null
++ set <-function(m1){
++ x<<-m1
++ n1 <<-NULL
++ }
++ get <-function()x
++ setinverse <-function(solve) n1 <<-solve
++ getinverse <-function ()n1
++ list (set =set, get=get, serinverse =setinverse, getinverse=getinverse)
++ }
+
+# The "cacheSolve" function in RStudio is a useful tool for calculating the inverse of a matrix and storing the result for future use. 
+# This function is especially beneficial when working with large matrices, as the process of calculating the inverse can be computationally intensive and time-consuming. 
+# By utilizing caching, the "cacheSolve" function saves the pre-calculated inverse, allowing subsequent calls to retrieve it instead of repeating the calculation. 
+# This caching mechanism significantly reduces computational time and improves efficiency when working with repetitive calculations on the same matrix.
+
+> cacheSolve <-function(x,...){
++ n1 <-x$getinverse()
++ if(!is.null(n1)){
++ message("getting cached data")
++ return(n1)}
++ data <-x$get()
++ n1 <-solve(data)
++ x$setinverse(n1)
++ n1
++ }
